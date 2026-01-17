8a298948ef66f78918df1f5fe18a0d06ba5eaa14
Gokul.B <156208804+gokullaxman@users.noreply.github.com>
2026-01-17 20:59:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -56,14 +56,14 @@
 const messages = [
     "Are you sure?",
     "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "confirmm ahhh?",
+    "Paatuuu please...",
+    "konjommm considerrr me!",
+    "If you say no, babyyy will be really sad...",
+    "babyyy will be very real sad...",
+    "husbandd will be very very very sad...",
+    "M, pothumm na pora...",
+    "chummaaa sonnaa, telll yes please! ❤️"
 ];
 
 let messageIndex = 0;
